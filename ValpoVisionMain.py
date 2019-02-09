6e166dc77f6beffb54c867fcc4f90c5e4fbe7f4c
jbayert <35811092+jbayert@users.noreply.github.com>
2019-02-08 19:33:15 -0600

    Add files via upload

--- ValpoVisionMain.py
+++ ValpoVisionMain.py
@@ -1,5 +1,4 @@
 # USAGE
-# python video.py --video FlightDemo.mp4
 area_min = 10
 res = (640,480)
 # import the necessary packages
@@ -10,6 +9,7 @@ from time import sleep
 from numpy import array
 from numpy import int0
 from numpy import argsort
+from get_line import *
 
 def crop(image,x,y,width,height):
     return image[y:y+height,x:x+width]
@@ -49,34 +49,7 @@ while True:
     (_,contours,_) = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,
         cv2.CHAIN_APPROX_SIMPLE)
 
-    for cnt in contours:
-        peri = cv2.arcLength(cnt, True)
-        approx = cv2.approxPolyDP(cnt, 0.05 * peri, True)
-        area = cv2.contourArea(cnt)
-        if len(approx) >= 4 and len(approx) <= 6 and area>area_min:
-            box = int0(cv2.boxPoints(cv2.minAreaRect(cnt) ))
-            print(box)
-            shifted_box = []
-            for a in box:
-                shifted_box.append([a[0]+x_box,a[1]+y_box])
-            shifted_box = int0(shifted_box)
-            sorted_box=shifted_box[argsort(shifted_box[:,1])]
-            top = sorted_box[0:2]
-            top_x = [n[0] for n in top]
-            top_x_left  = top_x[0] if top_x[0] < top_x[1] else top_x[1] 
-            top_x_right = top_x[0] if top_x[0] > top_x[1] else top_x[1]
-            bottom = sorted_box[2:4]
-            bottom_x = [n[0] for n in bottom]
-            bottom_x_left  = bottom_x[0] if bottom_x[0] < bottom_x[1] else bottom_x[1] 
-            bottom_x_right = bottom_x[0] if bottom_x[0] > bottom_x[1] else bottom_x[1]
-
-            top_x_center = (top_x_left + top_x_right)/2
-            bottom_x_center = (bottom_x_left + bottom_x_right)/2
-
-            cv2.line(image, (int(top_x_right), y_box), (int(bottom_x_right), y_box+height_box), (255,0,0), 2)
-            cv2.line(image, (int(top_x_center), y_box), (int(bottom_x_center), y_box+height_box), (0,255,0), 2)
-            cv2.line(image, (int(top_x_left), y_box), (int(bottom_x_left), y_box+height_box), (255,0,0), 2)
-    cv2.rectangle(image,(x_box,y_box),(x_box+width_box,y_box+height_box),(0,255,0),2)
+    pos = get_line(image,contours,box)
 
     
     cv2.imshow("Frame",image)
